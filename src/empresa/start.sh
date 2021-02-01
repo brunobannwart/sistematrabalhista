ad16079705295a864a2678d9f37f66c3f40e7e66
pedrosbo1 <pedro.catalini@hotmail.com>
2021-02-01 18:48:42 -0300

    files for deploy

---
+++ src/empresa/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo python3 /home/ubuntu/extcomp/sistematrabalhista/src/empresa/manage.py runserver 0.0.0.0:8002
+
---
+++ src/reconhecedor/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo python3 /home/ubuntu/extcomp/sistemaciapd/administrador/src/manage.py runserver 0.0.0.0:8000
+
