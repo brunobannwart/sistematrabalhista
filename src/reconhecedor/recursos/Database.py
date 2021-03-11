f32ada900f1e488ebc530308acb07774cb8c1f2e
root <root@ip-172-31-0-48.sa-east-1.compute.internal>
2021-03-11 21:37:14 +0000

    bugfix release do banco

--- src/reconhecedor/recursos/Database.py
+++ src/reconhecedor/recursos/Database.py
@@ -2,19 +2,25 @@ import mysql.connector as mysql
 
 class Database:
 	def __init__(self):
-		self.conexao = mysql.connect(host='localhost', user='tr_reconhecedor', password='reconhecedor$trabalhista', database='db_trabalhista', auth_plugin='mysql_native_password')
-		
-		self.cursor = self.conexao.cursor()
-		self.__criarTabela()
+		conexao = mysql.connect(host='db_trabalhista.mysql.dbaas.com.br', user='db_trabalhista', password='Myext123@', database='db_trabalhista', auth_plugin='mysql_native_password')
+		cursor = conexao.cursor()
 
-	def __criarTabela(self):
-		self.cursor.execute("CREATE TABLE IF NOT EXISTS `treinamento`(`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `foto` VARCHAR(100), `codificações` TEXT, `grupo` VARCHAR(100)) ENGINE=InnoDB")
+		self.__criarTabela(cursor)
+		
+		cursor.close()
+		conexao.close()
+
+	def __criarTabela(self, cursor):
+		cursor.execute("CREATE TABLE IF NOT EXISTS `treinamento`(`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `foto` VARCHAR(100), `codificações` TEXT, `grupo` VARCHAR(100)) ENGINE=InnoDB")
 
 	def obterFaces(self):
+		conexao = mysql.connect(host='db_trabalhista.mysql.dbaas.com.br', user='db_trabalhista', password='Myext123@', database='db_trabalhista', auth_plugin='mysql_native_password')
+		cursor = conexao.cursor()
+
 		faces = []
 
-		self.cursor.execute("SELECT * FROM `treinamento`")
-		resultados = self.cursor.fetchall()
+		cursor.execute("SELECT * FROM `treinamento`")
+		resultados = cursor.fetchall()
 
 		for resultado in resultados:
 			id_ = resultado[0]
@@ -29,19 +35,33 @@ class Database:
 				'grupo': grupo
 			}
 
-			faces.append(face)	
+			faces.append(face)
+		cursor.close()
+		conexao.close()	
 		return faces
 
 	def salvarFace(self, arquivo, codigos, grupo):
-		self.cursor.execute("INSERT INTO `treinamento`(`foto`, `codificações`, `grupo`) VALUES (%s, %s, %s)",
+		conexao = mysql.connect(host='db_trabalhista.mysql.dbaas.com.br', user='db_trabalhista', password='Myext123@', database='db_trabalhista', auth_plugin='mysql_native_password')
+		cursor = conexao.cursor()
+
+		cursor.execute("INSERT INTO `treinamento`(`foto`, `codificações`, `grupo`) VALUES (%s, %s, %s)",
 				[arquivo, codigos, grupo])
-		self.conexao.commit()
-		return self.cursor.lastrowid
+		conexao.commit()
+		cursor.close()
+		conexao.close()
+		return cursor.lastrowid
 
 	def deletarFace(self, id_):
+		conexao = mysql.connect(host='db_trabalhista.mysql.dbaas.com.br', user='db_trabalhista', password='Myext123@', database='db_trabalhista', auth_plugin='mysql_native_password')
+		cursor = conexao.cursor()
+
 		try:
-			self.cursor.execute("DELETE FROM `treinamento` WHERE `id`=%s", [id_])
-			self.conexao.commit()
+			cursor.execute("DELETE FROM `treinamento` WHERE `id`=%s", [id_])
+			conexao.commit()
+			cursor.close()
+			conexao.close() 
 			return True
 		except:
+			cursor.close()
+			conexao.close() 
 			return False
