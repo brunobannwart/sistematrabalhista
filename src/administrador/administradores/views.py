f25a03abeeeba9dc15976e372fd2a9ac6f59975d
brunobannwart <bbannwart@yahoo.com.br>
2021-04-18 11:37:49 -0300

    remove print

--- src/administrador/administradores/views.py
+++ src/administrador/administradores/views.py
@@ -53,8 +53,7 @@ def admincreate_view(request):
 					else:
 						formulario = request.POST
 						erro = 'Foto inválida'
-				except Exception as e:
-					print(e)
+				except:
 					formulario = request.POST
 					erro = 'Não foi possível cadastrar novo administrador'
 		else:
