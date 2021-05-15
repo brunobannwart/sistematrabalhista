a2c93d7b2281e2f85c49914056980ed176a022e4
brunobannwart <bbannwart@yahoo.com.br>
2021-05-15 09:55:10 -0300

    clean code

--- src/administrador/administradores/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-admin.site.site_header = 'Administrativo'
-admin.site.site_title = 'ExtComp'
-admin.site.index_title = 'Gerenciamento'
--- src/administrador/videoaulas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideoaulasConfig(AppConfig):
-    name = 'videoaulas'
