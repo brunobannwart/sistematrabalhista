bd27474ffb17778af410c6b2f20201437487e56e
Julia Gomes <juliamgomes1@gmail.com>
2021-06-24 12:47:00 -0300

    substituição do nome vitalita nos textos

--- src/administrador/administradores/admin.py
+++ src/administrador/administradores/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 admin.site.site_header = 'Administrativo'
-admin.site.site_title = 'Vitalità'
+admin.site.site_title = 'Administrativo'
 admin.site.index_title = 'Gerenciamento'
