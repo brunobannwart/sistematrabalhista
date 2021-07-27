6f2fda670eef8b2212ee90be3c7bacc7beebbbf4
Julia Gomes <juliamgomes1@gmail.com>
2021-07-27 19:04:20 -0300

    alteração no views.py

--- src/administrador/associados/views.py
+++ src/administrador/associados/views.py
@@ -192,43 +192,41 @@ def associatedpdf_view(request, id=0):
 	if id == 0:
 		return redirect('associatedlist')
 
-	with connection.cursor() as cursor:
-		cursor.execute("SELECT * FROM associado WHERE id=%s", [id])
-		resultado = cursor.fetchone()
+	try:
+		associado = Associado.objects.get(id=id)
+		formulario = {
+			'id': id,
+			'foto': settings.MEDIA_ROOT + '/' + associado.foto.name,
+			'nome': associado.nome,
+			'data_nascimento': associado.data_nascimento,
+			'cpf': associado.cpf,
+			'celular': associado.celular,
+			'email': associado.email,
+			'cep': associado.cep,
+			'numero': associado.numero,
+			'pcd': associado.pcd,
+			'outras_informacoes': associado.outras_informacoes,
+			'instituicao_ensino': associado.instituicao_ensino,
+			'curso_extra': associado.curso_extra,
+			'empresa_trabalhada': associado.empresa_trabalhada,
+			'cargo_ocupado': associado.cargo_ocupado,
+		}
 
-		if resultado != None:
-			formulario = {
-				'id': resultado[0],
-				'foto': settings.MEDIA_ROOT + '/' + resultado[1],
-				'nome': resultado[2],
-				'data_nascimento': resultado[3],
-				'cpf': resultado[4],
-				'celular': resultado[5],
-				'email': resultado[6],
-				'cep': resultado[8],
-				'numero': resultado[9],
-				'pcd': resultado[10],
-				'outras_informacoes': resultado[11],
-				'instituicao_ensino': resultado[12],
-				'curso_extra': resultado[13],
-				'empresa_trabalhada': resultado[14],
-				'cargo_ocupado': resultado[15],
-			}
+		contexto = {
+			'form': formulario,
+		}
 
-			contexto = {
-				'form': formulario,
-			}
+		pdf = render_to_pdf('associated/pdf.html', contexto)
 
-			pdf = render_to_pdf('associated/pdf.html', contexto)
+		if pdf:
+			resposta = HttpResponse(pdf, content_type='application/pdf')
+			nomearquivo = associado.nome.replace(' ', '_').lower() + '.pdf'
+			conteudo = 'inline; filename=%s' % (nomearquivo)
+			resposta['Content-Disposition'] = conteudo
+			return resposta
 
-			if pdf:
-				resposta = HttpResponse(pdf, content_type='application/pdf')
-				nomearquivo = resultado[2].replace(' ', '_').lower() + '.pdf'
-				conteudo = 'inline; filename=%s' % (nomearquivo)
-				resposta['Content-Disposition'] = conteudo
-				return resposta
-
-			else:
-				return redirect('associatedlist', id=id)
 		else:
-			return redirect('associatedlist')
+			return redirect('associatedlist', id=id)
+
+	except:
+		return redirect('associatedlist')
